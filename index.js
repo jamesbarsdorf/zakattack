f695373813dac09b365f502aac5d37eca553a993
jamesbarsdorf <james@nanobox.io>
2018-10-23 13:33:23 -0600

    first change to skitten

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+// this makes public folder default source
 app.use(express.static('public'));
 
+// these are the routes for request and response from server
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/public/index.html');
 });
@@ -11,6 +13,7 @@ app.get('/Your', function(req, res){
   res.send('Mom!');
 });
 
+// This creates our express server and assigns a port to listen on
 app.listen(process.env.PORT || 4000, function(){
   console.log('app is listening on port 4000');
 });
